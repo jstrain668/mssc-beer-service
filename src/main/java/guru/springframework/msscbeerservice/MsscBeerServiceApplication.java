bd23e65a460bd7709fc67e8c68a32c50fdd4a2d1
Developer <jjstrain68@gmail.com>
2021-01-27 11:34:05 +0000

    sfg-brewery-bom version is 1.0.11 in pom.xml

--- src/main/java/guru/springframework/msscbeerservice/MsscBeerServiceApplication.java
+++ src/main/java/guru/springframework/msscbeerservice/MsscBeerServiceApplication.java
@@ -2,9 +2,11 @@ package guru.springframework.msscbeerservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+//import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 
 @SpringBootApplication
+//@EnableDiscoveryClient
 public class MsscBeerServiceApplication {
 
     public static void main(String[] args) {
